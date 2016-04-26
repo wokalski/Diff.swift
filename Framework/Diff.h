288bce117f52f6557bdf7e4cf4913bbfbcd3483f
Wojtek Czekalski <me@wczekalski.com>
2016-04-26 21:54:30 +0200

    Fixed framework Xcode project pointing to nonexistent files

--- Framework/Diff.h
+++ Framework/Diff.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 wczekalski. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Diff.swift.
 FOUNDATION_EXPORT double Diff_swiftVersionNumber;
