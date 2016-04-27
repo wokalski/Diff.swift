322bd1bc5fa1ac647295e68db08577e3fe25bf66
Wojtek Czekalski <me@wczekalski.com>
2016-04-27 09:00:13 +0200

    Removed Carthage checkouts from Benchmark project

--- Benchmark/Carthage/Checkouts/Diff.swift/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Diff",
-    dependencies: []
-    )
-
-products.append(Product(name:"Diff", type: .Library(.Dynamic), modules: ["Diff"]))
--- Benchmark/Carthage/Checkouts/Diff.swift/README.md
+++
@@ -1,2 +0,0 @@
-# Diff.swift
-Diff any CollectionType&lt;T> where T is equatable
