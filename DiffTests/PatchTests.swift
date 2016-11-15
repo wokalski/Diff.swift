c4e5fd17375f3a439319ed9343d4e73295574029
Wojtek Czekalski <me@wczekalski.com>
2016-11-15 12:15:52 +0100

    Makes insertion and deletion first patch reordering work

--- DiffTests/PatchTests.swift
+++ DiffTests/PatchTests.swift
@@ -1,73 +1,133 @@
 import XCTest
 import Diff
 
-let defaultOrder = [
-    ("kitten", "sitting", "D(0)I(0,s)D(4)I(4,i)I(6,g)"),
-    ("🐩itt🍨ng", "kitten", "D(0)I(0,k)D(4)I(4,e)D(6)"),
-    ("1234", "ABCD", "D(0)D(0)D(0)D(0)I(0,A)I(1,B)I(2,C)I(3,D)"),
-    ("1234", "", "D(0)D(0)D(0)D(0)"),
-    ("", "1234", "I(0,1)I(1,2)I(2,3)I(3,4)"),
-    ("Hi", "Oh Hi", "I(0,O)I(1,h)I(2, )"),
-    ("Hi", "Hi O", "I(2, )I(3,O)"),
-    ("Hi O", "Hi", "D(2)D(2)"),
-    ("Wojtek", "Wojciech", "D(3)I(3,c)I(4,i)D(6)I(6,c)I(7,h)"),
-    ("1234", "1234", ""),
-    ("", "", ""),
-    ("Oh Hi", "Hi Oh", "D(0)D(0)D(0)I(2, )I(3,O)I(4,h)"),
-    ("1362", "31526", "D(0)D(1)I(1,1)I(2,5)I(4,6)")
-]
-
-let insertionsFirst = [
-    ("kitten", "sitting", "I(0,s)I(4,i)I(6,g)D(1)D(5)"),
-    ("🐩itt🍨ng", "kitten", "I(0,k)I(5,e)D(1)D(6)D(8)"),
-    ("1234", "ABCD", "I(0,A)I(1,B)I(2,C)I(3,D)D(4)D(5)D(6)D(7)"),
-    ("1234", "", "D(0)D(1)D(2)D(3)"),
-    ("", "1234", "I(0,1)I(1,2)I(2,3)I(3,4)"),
-    ("Hi", "Oh Hi", "I(0,O)I(1,h)I(2, )"),
-    ("Hi", "Hi O", "I(2, )I(3,O)"),
-    ("Hi O", "Hi", "D(2)D(3)"),
-    ("Wojciechk", "Wojciech", "I(3,c)I(4,i)I(6,c)I(7,h)D(5)D(8)"),
-    ("1234", "1234", ""),
-    ("", "", ""),
-    ("Oh Hi", "Hi Oh", "I(5, )I(6,O)I(7,h)D(0)D(1)D(2)"),
-    ("1362", "31526", "I(2,1)I(3,5)I(4,6)D(0)D(4)")
-]
-
-let deletionsFirst = [
-    ("kitten", "sitting", "I(0,s)I(4,i)I(6,g)D(1)D(5)"),
-    ("🐩itt🍨ng", "kitten", "I(0,k)I(5,e)D(1)D(6)D(8)"),
-    ("1234", "ABCD", "I(0,A)I(1,B)I(2,C)I(3,D)D(4)D(5)D(6)D(7)"),
-    ("1234", "", "D(0)D(1)D(2)D(3)"),
-    ("", "1234", "I(0,1)I(1,2)I(2,3)I(3,4)"),
-    ("Hi", "Oh Hi", "I(0,O)I(1,h)I(2, )"),
-    ("Hi", "Hi O", "I(2, )I(3,O)"),
-    ("Hi O", "Hi", "D(2)D(3)"),
-    ("Wojciech", "Wojciech", "I(3,c)I(4,i)I(6,c)I(7,h)D(5)D(8)"),
-    ("1234", "1234", ""),
-    ("", "", ""),
-    ("Oh Hi", "Hi Oh", "I(5, )I(6,O)I(7,h)D(0)D(1)D(2)"),
-    ("1362", "31526", "I(2,1)I(3,5)I(4,6)D(0)D(4)")
-]
-
 class PatchTests: XCTestCase {
 
     func testDefaultOrder() {
+        
+        let defaultOrder = [
+            ("kitten", "sitting", "D(0)I(0,s)D(4)I(4,i)I(6,g)"),
+            ("🐩itt🍨ng", "kitten", "D(0)I(0,k)D(4)I(4,e)D(6)"),
+            ("1234", "ABCD", "D(0)D(0)D(0)D(0)I(0,A)I(1,B)I(2,C)I(3,D)"),
+            ("1234", "", "D(0)D(0)D(0)D(0)"),
+            ("", "1234", "I(0,1)I(1,2)I(2,3)I(3,4)"),
+            ("Hi", "Oh Hi", "I(0,O)I(1,h)I(2, )"),
+            ("Hi", "Hi O", "I(2, )I(3,O)"),
+            ("Hi O", "Hi", "D(2)D(2)"),
+            ("Wojtek", "Wojciech", "D(3)I(3,c)I(4,i)D(6)I(6,c)I(7,h)"),
+            ("1234", "1234", ""),
+            ("", "", ""),
+            ("Oh Hi", "Hi Oh", "D(0)D(0)D(0)I(2, )I(3,O)I(4,h)"),
+            ("1362", "31526", "D(0)D(1)I(1,1)I(2,5)I(4,6)"),
+            ("1234b2", "ab", "D(0)D(0)D(0)D(0)I(0,a)D(2)")
+        ]
+        
         for expectation in defaultOrder {
             XCTAssertEqual(
                 _test(expectation.0, to: expectation.1),
                 expectation.2)
         }
     }
+    
+    func testInsertionsFirst() {
+        
+        let insertionsFirst = [
+            ("kitten", "sitting", "I(0,s)I(5,i)I(8,g)D(1)D(5)"),
+            ("🐩itt🍨ng", "kitten", "I(0,k)I(5,e)D(1)D(5)D(6)"),
+            ("1234", "ABCD", "I(0,A)I(1,B)I(2,C)I(3,D)D(4)D(4)D(4)D(4)"),
+            ("1234", "", "D(0)D(0)D(0)D(0)"),
+            ("", "1234", "I(0,1)I(1,2)I(2,3)I(3,4)"),
+            ("Hi", "Oh Hi", "I(0,O)I(1,h)I(2, )"),
+            ("Hi", "Hi O", "I(2, )I(3,O)"),
+            ("Hi O", "Hi", "D(2)D(2)"),
+            ("Wojtek", "Wojciech", "I(3,c)I(4,i)I(7,c)I(8,h)D(5)D(8)"),
+            ("1234", "1234", ""),
+            ("", "", ""),
+            ("Oh Hi", "Hi Oh", "I(5, )I(6,O)I(7,h)D(0)D(0)D(0)"),
+            ("1362", "31526", "I(2,1)I(3,5)I(6,6)D(0)D(3)")
+        ]
+        
+        let insertionsFirstSort = { (element1: DiffElement, element2: DiffElement) -> Bool in
+            switch (element1, element2) {
+            case (.insert(let at1), .insert(let at2)):
+                return at1 < at2
+            case (.insert, .delete):
+                return true
+            case (.delete, .insert):
+                return false
+            case (.delete(let at1), .delete(let at2)):
+                return at1 < at2
+            default: fatalError()
+            }
+        }
+        
+        for expectation in insertionsFirst {
+            XCTAssertEqual(
+                _test(
+                    expectation.0,
+                    to: expectation.1,
+                    sortingFunction: insertionsFirstSort),
+                expectation.2)
+        }
+    }
+    
+    func testDeletionsFirst() {
+        
+        let deletionsFirst = [
+            ("kitten", "sitting", "D(0)D(3)I(0,s)I(4,i)I(6,g)"),
+            ("🐩itt🍨ng", "kitten", "D(0)D(3)D(4)I(0,k)I(4,e)"),
+            ("1234", "ABCD", "D(0)D(0)D(0)D(0)I(0,A)I(1,B)I(2,C)I(3,D)"),
+            ("1234", "", "D(0)D(0)D(0)D(0)"),
+            ("", "1234", "I(0,1)I(1,2)I(2,3)I(3,4)"),
+            ("Hi", "Oh Hi", "I(0,O)I(1,h)I(2, )"),
+            ("Hi", "Hi O", "I(2, )I(3,O)"),
+            ("Hi O", "Hi", "D(2)D(2)"),
+            ("Wojtek", "Wojciech", "D(3)D(4)I(3,c)I(4,i)I(6,c)I(7,h)"),
+            ("1234", "1234", ""),
+            ("", "", ""),
+            ("Oh Hi", "Hi Oh", "D(0)D(0)D(0)I(2, )I(3,O)I(4,h)"),
+            ("1362", "31526", "D(0)D(1)I(1,1)I(2,5)I(4,6)")
+        ]
+        
+        let deletionsFirstSort = { (element1: DiffElement, element2: DiffElement) -> Bool in
+            switch (element1, element2) {
+            case (.insert(let at1), .insert(let at2)):
+                return at1 < at2
+            case (.insert, .delete):
+                return false
+            case (.delete, .insert):
+                return true
+            case (.delete(let at1), .delete(let at2)):
+                return at1 < at2
+            default: fatalError()
+            }
+        }
+        
+        for expectation in deletionsFirst {
+            XCTAssertEqual(
+                _test(
+                    expectation.0,
+                    to: expectation.1,
+                    sortingFunction: deletionsFirstSort),
+                expectation.2)
+        }
+    }
 }
+
+typealias SortingFunction = (DiffElement, DiffElement) -> Bool
 
 func _test(
     _ from: String,
-    to: String) -> String {
+    to: String,
+    sortingFunction: SortingFunction? = nil) -> String {
     return from
         .diff(to)
-        .patch(from.characters, b: to.characters)
+        .patch(
+            from.characters,
+            b: to.characters,
+            sort: sortingFunction)
         .reduce("") { $0 + $1.debugDescription }
-    
+
 }
 
 
--- Sources/Patch.swift
+++ Sources/Patch.swift
@@ -1,35 +1,3 @@
-func valuesSortedByKey<T>(_ dict: [Int:[T]]) -> [T] {
-    let keys = dict.keys.sorted()
-    return keys.flatMap { (key: Int) -> [T] in return dict[key]! }
-}
-
-public extension Diff {
-    
-    typealias OrderedBefore = (_ fst: DiffElement, _ snd: DiffElement) -> Bool
-    
-    public func patch<T: Collection>(
-        _ a: T,
-        b: T
-        ) -> [PatchElement<T.Iterator.Element>] where T.Iterator.Element : Equatable {
-        
-        typealias PE = PatchElement<T.Generator.Element>
-        
-        var shift = 0
-        return self.indices.map { i in
-            let element = self[i]
-            switch element {
-            case let .delete(at):
-                shift -= 1
-                return .deletion(index: at+shift+1)
-            case let .insert(at):
-                shift += 1
-                return .insertion(index: at, element: b.itemOnStartIndex(advancedBy: at))
-            }
-        }
-
-        
-    }
-}
 
 public enum PatchElement<Element> {
     case insertion(index: Int, element: Element)
@@ -51,6 +19,144 @@ public enum ExtendedPatch<Element, Index> {
     case move(from: Index, to: Index)
 }
 
+public extension Diff {
+    
+    typealias OrderedBefore = (_ fst: DiffElement, _ snd: DiffElement) -> Bool
+
+    public func patch<T: Collection>(
+        _ a: T,
+        b: T,
+        sort: OrderedBefore? = nil
+        ) -> [PatchElement<T.Iterator.Element>] where T.Iterator.Element : Equatable {
+        
+        typealias PE = PatchElement<T.Generator.Element>
+
+        var shift = 0
+        let shiftedDiff: [PE] = self.indices.map { i in
+            let element = self[i]
+            switch element {
+            case let .delete(at):
+                shift -= 1
+                return .deletion(index: at+shift+1)
+            case let .insert(at):
+                shift += 1
+                return .insertion(index: at, element: b.itemOnStartIndex(advancedBy: at))
+            }
+        }
+        
+        guard let sort = sort else {
+            return shiftedDiff
+        }
+        
+        let sortedDiff = indices.map { index in
+            return (self[index], index)
+        }.sorted { (fst, snd) -> Bool in
+            return sort(fst.0, snd.0)
+        }.map { (shiftedDiff[$0.1], $0.1) }
+        
+        
+        let sortedDiffToShiftedDiffIndex =
+            zip(sortedDiff.map { $0.1 }, sortedDiff.indices)
+                .sorted { $0.0 < $1.0 }
+                .map { $0.1 }
+        
+        let sortedPatchElements = shiftedDiff.indices.map {
+            PatchReorderedElement(
+                value: shiftedDiff[$0],
+                oldIndex: $0,
+                newIndex: sortedDiffToShiftedDiffIndex[$0]
+            )
+        }
+        
+        let linkedList = DoublyLinkedList(linkedList: LinkedList(array: sortedPatchElements))
+        if let secondElement = linkedList?.next {
+            process(node: secondElement)
+        }
+        
+        guard let result = linkedList?.array().sorted(by: { (fst, second) -> Bool in
+            return fst.newIndex < second.newIndex
+        }) else {
+            return shiftedDiff
+        }
+        return result.map { $0.value }
+    }
+}
+
+// MARK: Patch reordering
+
+enum Direction {
+    case left
+    case right
+}
+
+enum EdgeType {
+    case cycle
+    case neighbor(direction: Direction)
+    case jump(direction: Direction)
+}
+
+func edgeDirection<T>(from: DoublyLinkedList<PatchReorderedElement<T>>, to: DoublyLinkedList<PatchReorderedElement<T>>) -> EdgeType {
+    let fromIndex = from.value.newIndex
+    let toIndex = to.value.newIndex
+    
+    if fromIndex == toIndex {
+        return .cycle
+    } else if abs(fromIndex - toIndex) == 1 {
+        if fromIndex > toIndex {
+            return .neighbor(direction: .left)
+        } else {
+            return .neighbor(direction: .right)
+        }
+    } else if fromIndex > toIndex {
+        return .jump(direction: .left)
+    } else {
+        return .jump(direction: .right)
+    }
+}
+
+func process<T>(node: DoublyLinkedList<PatchReorderedElement<T>>) {
+    var from = node.previous
+    while let nextFrom = from, nextFrom.value.oldIndex < node.value.oldIndex {
+        process(from: nextFrom, to: node)
+        from = nextFrom.previous
+    }
+    
+    if let next = node.next {
+        process(node: next)
+    }
+}
+
+func process<T>(from: DoublyLinkedList<PatchReorderedElement<T>>, to: DoublyLinkedList<PatchReorderedElement<T>>) {
+    switch edgeDirection(from: from, to: to) {
+    case .cycle:
+        fatalError()
+    case .neighbor(let direction), .jump(let direction):
+        if case .left = direction {
+            switch (from.value.value, to.value.value) {
+            case (.insertion, .insertion):
+                break
+            case (.deletion, .deletion):
+                break
+            case (.insertion, .deletion(let position)):
+                to.value = PatchReorderedElement(value: .deletion(index: position - 1), oldIndex: to.value.oldIndex, newIndex: to.value.newIndex)
+            case (.deletion(let dPosition), .insertion(let iPosition, let element)):
+                if dPosition == iPosition {
+                    from.value = PatchReorderedElement(value: .deletion(index: dPosition + 1), oldIndex: from.value.oldIndex, newIndex: from.value.newIndex)
+                } else if dPosition < iPosition {
+                    to.value = PatchReorderedElement(value: .insertion(index: iPosition + 1, element: element), oldIndex: to.value.oldIndex, newIndex: to.value.newIndex)
+                }
+            }
+        }
+    }
+}
+
+struct PatchReorderedElement<T> {
+    var value: PatchElement<T>
+    let oldIndex: Int
+    let newIndex: Int
+
+}
+
 extension PatchElement: CustomDebugStringConvertible {
     public var debugDescription: String {
         switch self {
@@ -61,21 +167,3 @@ extension PatchElement: CustomDebugStringConvertible {
         }
     }
 }
-
-//public extension RangeReplaceableCollectionType where Self.Generator.Element : Equatable, Self.Index : SignedIntegerType {
-//    
-//    public func apply(patch: [PatchElement<Generator.Element>]) -> Self {
-//        var mutableSelf = self
-//        
-//        for change in patch {
-//            switch change {
-//            case let .insertion(index, element):
-//                mutableSelf.insert(element, atIndex: index)
-//            case let .deletion(index):
-//                mutableSelf.removeAtIndex(index)
-//            }
-//        }
-//        
-//        return mutableSelf
-//    }
-//}
