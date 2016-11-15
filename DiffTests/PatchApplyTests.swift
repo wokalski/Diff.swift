dfca70d132d05d6613ba611bf379cd73bd5eadab
Wojtek Czekalski <me@wczekalski.com>
2016-11-15 17:08:38 +0100

    Adds patch applying

---
+++ DiffTests/PatchApplyTests.swift
@@ -0,0 +1,72 @@
+import XCTest
+import Diff
+
+class PatchApplyTests: XCTestCase {
+    func testString() {
+        
+        let testCases: [(String, String, String)] = [
+            ("", "I(0,A)I(0,B)I(0,C)", "CBA"),
+            ("", "I(0,A)I(1,B)I(1,C)", "ACB"),
+            ("AB", "D(1)I(1,B)I(1,C)", "ACB"),
+            ("AB", "I(1,B)D(0)I(1,C)", "BCB"),
+            ("A", "I(0,B)D(0)", "A")
+        ]
+        
+        testCases.forEach { (seed, patchString, result) in
+            XCTAssertEqual(seed.apply(stringPatch(from: patchString)), result)
+        }
+    }
+    
+    func testCollection() {
+        
+        let testCases: [([Int], String, [Int])] = [
+            ([], "I(0,0)I(0,1)I(0,2)", [2, 1, 0]),
+            ([], "I(0,0)I(1,1)I(1,2)", [0, 2, 1]),
+            ([0, 1], "D(1)I(1,1)I(1,2)", [0, 2, 1]),
+            ([0, 1], "I(1,1)D(0)I(1,2)", [1, 2, 1]),
+            ([0], "I(0,1)D(0)", [0])
+        ]
+        
+        testCases.forEach { (seed, patchString, result) in
+            XCTAssertEqual(seed.apply(intPatch(from: patchString)), result)
+        }
+    }
+}
+
+func stringPatch(from textualRepresentation: String) -> [PatchElement<String.CharacterView.Iterator.Element>] {
+    return textualRepresentation.components(separatedBy: ")").flatMap { string in
+        if string == "" {
+            return nil
+        }
+        let type = string.substring(to: string.index(after: string.startIndex))
+        if type == "D" {
+            let index = Int(string.substring(from: string.index(string.startIndex, offsetBy: 2)))!
+            return .deletion(index: index)
+        } else if type == "I" {
+            let indexAndElement = string.substring(from: string.index(string.startIndex, offsetBy: 2)).components(separatedBy: ",")
+            return .insertion(index: Int(indexAndElement[0])!, element: indexAndElement[1].characters.first!)
+        } else {
+            return nil
+        }
+    }
+}
+
+func intPatch(from textualRepresentation: String) -> [PatchElement<Int>] {
+    return textualRepresentation.components(separatedBy: ")").flatMap { string in
+        if string == "" {
+            return nil
+        }
+        let type = string.substring(to: string.index(after: string.startIndex))
+        if type == "D" {
+            let index = Int(string.substring(from: string.index(string.startIndex, offsetBy: 2)))!
+            return .deletion(index: index)
+        } else if type == "I" {
+            let indexAndElement = string.substring(from: string.index(string.startIndex, offsetBy: 2)).components(separatedBy: ",")
+            let index = Int(indexAndElement[0])!
+            let element = Int(indexAndElement[1])!
+            return .insertion(index: index, element: element)
+        } else {
+            return nil
+        }
+    }
+}
---
+++ Sources/Patch+Apply.swift
@@ -0,0 +1,41 @@
+
+public extension RangeReplaceableCollection where Self.Iterator.Element : Equatable {
+
+    public func apply(_ patch: [PatchElement<Generator.Element>]) -> Self {
+        var mutableSelf = self
+
+        for change in patch {
+            switch change {
+            case let .insertion(i, element):
+                let target = mutableSelf.index(mutableSelf.startIndex, offsetBy: IndexDistance(IntMax(i)))
+                mutableSelf.insert(element, at: target)
+            case let .deletion(i):
+                let target = mutableSelf.index(mutableSelf.startIndex, offsetBy: IndexDistance(IntMax(i)))
+                mutableSelf.remove(at: target)
+            }
+        }
+
+        return mutableSelf
+    }
+}
+
+public extension String {
+    
+    public func apply(_ patch: [PatchElement<String.CharacterView.Iterator.Element>]) -> String {
+        var mutableSelf = self
+        
+        for change in patch {
+            switch change {
+            case let .insertion(i, element):
+                let target = mutableSelf.index(mutableSelf.startIndex, offsetBy: IndexDistance(IntMax(i)))
+                mutableSelf.insert(element, at: target)
+            case let .deletion(i):
+                let target = mutableSelf.index(mutableSelf.startIndex, offsetBy: IndexDistance(IntMax(i)))
+                mutableSelf.remove(at: target)
+            }
+        }
+        
+        return mutableSelf
+    }
+}
+
