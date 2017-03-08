34d92fa7cbafc4db2a9ef29f0463a0e35d66058c
FarzadShbfn <farzad.shbfn@gmail.com>
2017-03-08 16:40:21 +0330

    swiftformat on LinkedList.swift

--- Sources/LinkedList.swift
+++ Sources/LinkedList.swift
@@ -9,32 +9,32 @@ class LinkedList<T> {
     }
 
     init?(array: [T]) {
-		let reversed = Array(array.reversed())
+        let reversed = Array(array.reversed())
         guard let first = array.first else {
             return nil
         }
-		
-		var tailLinkedList: LinkedList? = nil
-		
-		for i in 0 ..< reversed.count - 1 {
-			tailLinkedList = LinkedList(next: tailLinkedList, value: reversed[i])
-		}
-		
-		self.next = tailLinkedList
-		self.value = first
+
+        var tailLinkedList: LinkedList?
+
+        for i in 0 ..< reversed.count - 1 {
+            tailLinkedList = LinkedList(next: tailLinkedList, value: reversed[i])
+        }
+
+        self.next = tailLinkedList
+        self.value = first
     }
-	
-	func array() -> Array<T> {
-		if let next = next {
-			return [value] + next.array()
-		}
-		return [value]
-	}
+
+    func array() -> Array<T> {
+        if let next = next {
+            return [value] + next.array()
+        }
+        return [value]
+    }
 }
 
 class DoublyLinkedList<T> {
     let next: DoublyLinkedList?
-    private(set) var previous: DoublyLinkedList? = nil
+    private(set) var previous: DoublyLinkedList?
     var head: DoublyLinkedList {
         guard let previous = previous else {
             return self
@@ -43,38 +43,37 @@ class DoublyLinkedList<T> {
     }
 
     var value: T
-	
-	init(next: DoublyLinkedList?, value: T) {
-		self.value = value
-		self.next = next
-		self.next?.previous = self
-	}
-	
-	
-	init?(array: [T]) {
-		let reversed = Array(array.reversed())
-		guard let first = array.first else {
-			return nil
-		}
-		
-		var tailDoublyLinkedList: DoublyLinkedList? = nil
-		
-		for i in 0 ..< reversed.count - 1 {
-			let nextTail = DoublyLinkedList(next: tailDoublyLinkedList, value: reversed[i])
-			tailDoublyLinkedList?.previous = nextTail
-			tailDoublyLinkedList = nextTail
-		}
-		
-		self.value = first
-		self.next = tailDoublyLinkedList
-		self.next?.previous = self
-	}
+
+    init(next: DoublyLinkedList?, value: T) {
+        self.value = value
+        self.next = next
+        self.next?.previous = self
+    }
+
+    init?(array: [T]) {
+        let reversed = Array(array.reversed())
+        guard let first = array.first else {
+            return nil
+        }
+
+        var tailDoublyLinkedList: DoublyLinkedList?
+
+        for i in 0 ..< reversed.count - 1 {
+            let nextTail = DoublyLinkedList(next: tailDoublyLinkedList, value: reversed[i])
+            tailDoublyLinkedList?.previous = nextTail
+            tailDoublyLinkedList = nextTail
+        }
+
+        self.value = first
+        self.next = tailDoublyLinkedList
+        self.next?.previous = self
+    }
 
     convenience init?(linkedList: LinkedList<T>?) {
-		guard let linkedList = linkedList else {
-			return nil
-		}
-		self.init(array: linkedList.array())
+        guard let linkedList = linkedList else {
+            return nil
+        }
+        self.init(array: linkedList.array())
     }
 
     func array() -> Array<T> {
